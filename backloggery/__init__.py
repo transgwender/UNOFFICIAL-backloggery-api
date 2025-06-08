29db74c8b13a260c6748938397332b2d8edd37cc
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-06-07 23:09:46 -0700

    Version

--- backloggery/__init__.py
+++ backloggery/__init__.py
@@ -5,7 +5,7 @@ Unofficial Backloggery API Wrapper
 __title__ = 'backloggery'
 __author__ = 'transgwender'
 __license__ = 'MIT'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __status__ = 'Development'
 
 import logging
