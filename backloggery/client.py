55f1b744742f01ca8a2a74138dd2dc58f48108b0
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-06-10 21:59:55 -0700

    Major refactoring of error handling, as well as addition of fetch game call

--- backloggery/client.py
+++ backloggery/client.py
@@ -25,6 +25,7 @@ DEALINGS IN THE SOFTWARE.
 import datetime
 import json
 import re
+from datetime import datetime
 from typing import Dict, Any, Optional, List
 from urllib import request
 from urllib.error import HTTPError
@@ -32,8 +33,20 @@ from urllib.error import HTTPError
 import backloggery
 from backloggery.enums import *
 
+class NoDataFoundError(LookupError):
+    """ No data found during lookup. """
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
 
 class RawGame:
+    """
+    The raw game data
+    """
     def __init__(self, **options: Any):
         for key, value in options.items():
             setattr(self, key, value)
@@ -52,6 +65,10 @@ class RawGame:
         return r
 
 class Game(RawGame):
+    """
+    The processed game data
+    """
+
     def __init__(self, **options: Any):
         super().__init__(**options)
         if hasattr(self, 'status'):
@@ -69,12 +86,50 @@ class Game(RawGame):
         if hasattr(self, 'difficulty'):
             setattr(self, 'difficulty', cast(Difficulty, getattr(self, 'difficulty')))
 
-class GameCache:
-    def __init__(self, time: datetime.datetime, data: List[Game]):
+class LibraryCache:
+    """
+    The cache of game library data
+
+    :var time: datetime - The timestamp of the cache
+    :var data: List[Game] - The game library data
+    """
+    def __init__(self, time: datetime, data: List[Game]):
         self.time = time
         self.data = data
 
-def fetch(username: str) -> HTTPError | GameCache:
+def fetch_gameinfo(game_inst_id: int) -> Game:
+    """
+    Preforms a fetch game info call to Backloggery.
+
+    :param game_inst_id: The id of the game instance to fetch.
+    :raises NoDataFoundError: If no data was found for the given id.
+    :return: The game data received for the given id.
+    """
+
+    data = {'game_inst_id': game_inst_id}
+    data = json.dumps(data).encode('utf-8')
+    req = request.Request("https://backloggery.com/api/fetch_gameinfo.php", data=data)
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('Accept', 'application/json')
+    req.add_header('User-Agent', f'Backloggery Unofficial API Client/{backloggery.__version__} (dev[at]gwenkornak.ca)')
+    # req.add_header('If-Modified-Since', 'Thu, 05 Jun 2025 04:14:34 GMT')
+    resp = request.urlopen(req)
+    decoded = json.loads(resp.read().decode('utf-8'))
+    if not decoded:
+        raise NoDataFoundError(f'No Data Found for {game_inst_id}')
+    result = decoded['payload']
+    game = Game(**result)
+    return game
+
+def fetch_library(username: str) -> LibraryCache:
+    """
+    Preforms a fetch library call to Backloggery.
+
+    :param username: The username of the user to fetch.
+    :raises NoDataFoundError: If no results were found for the given username.
+    :return: The library data received for the given username.
+    """
+
     data = {'type': "load_user_library", 'username': username}
     data = json.dumps(data).encode('utf-8')
     req = request.Request("https://backloggery.com/api/fetch_library.php", data=data)
@@ -82,37 +137,70 @@ def fetch(username: str) -> HTTPError | GameCache:
     req.add_header('Accept', 'application/json')
     req.add_header('User-Agent', f'Backloggery Unofficial API Client/{backloggery.__version__} (dev[at]gwenkornak.ca)')
     # req.add_header('If-Modified-Since', 'Thu, 05 Jun 2025 04:14:34 GMT')
-    try:
-        resp = request.urlopen(req)
-    except HTTPError as err:
-        return err
+    resp = request.urlopen(req)
     decoded = json.loads(resp.read().decode('utf-8'))
     if not decoded:
-        return HTTPError(resp.url, 404, f'No Data Found for {username}', resp.headers, resp.fp)
+        raise NoDataFoundError(f'No Data Found for {username}')
     result = decoded['payload']
-    gc = GameCache(datetime.datetime.now(), [Game(**dct) for dct in result])
+    gc = LibraryCache(datetime.datetime.now(), [Game(**dct) for dct in result])
     return gc
 
 
 class BacklogClient:
+    """
+    Client to interact with Backlogger API.
+    """
     def __init__(self) -> None:
-        self.cache: Dict[str, GameCache] = {}
+        self.cache: Dict[str, LibraryCache] = {}
 
     def refresh_cache(self, username: str) -> None:
-        fet = fetch(username)
-        if isinstance(fet, HTTPError):
-            raise fet
+        """
+        Refreshes the cache for the given username.
+
+        :raises NoDataFoundError: If no data was found for the specified username.
+        :param username: The username to refresh.
+        """
+        fet = fetch_library(username)
         self.cache[username] = fet
 
-    def get_library(self, username: str) -> GameCache:
+    def get_game(self, game_inst_id: int) -> Game:
+        """
+        Gets the details of the specified game instance.
+
+        :param game_inst_id: The game instance ID
+        :raises NoDataFoundError: If no data was found for the specified game instance.
+        :return: The game data corresponding to the game instance ID
+        """
+
+        fet = fetch_gameinfo(game_inst_id)
+        return fet
+
+    def get_library(self, username: str) -> LibraryCache:
+        """
+        Gets the library of the specified user. Prioritizes the cached data if exists.
+
+        :param username: The username to look for
+        :raises NoDataFoundError: If no data was found for the specified username.
+        :return: The cache of the username's library games.
+        """
         if username not in self.cache:
             self.refresh_cache(username)
         return self.cache[username]
 
-    def search_library(self, username: str, search_regex: str, partial_match: bool = False) -> List[Game]:
+
+    def search_library(self, username: str, search_regex: str, partial_match: bool = False) -> tuple[datetime, list[Game]]:
+        """
+        Searches the specified library for games that match the given regex.
+
+        :param username: The username to search the library of.
+        :param search_regex: A json containing parameters and the regular expression to compare against.
+        :param partial_match: If True, return games that match any one of the regular expression. If False, return games that match every regular expression.
+        :raises NoDataFoundError: If no data was found for the specified username.
+        :return: A tuple containing both the time the library was fetched, and a list of the games found, if any.
+        """
         regex = json.loads(search_regex)
         lib = self.get_library(username)
         if partial_match:
-            return [g for g in lib.data if g.compare_any(**regex)]
+            return lib.time, [g for g in lib.data if g.compare_any(**regex)]
         else:
-            return [g for g in lib.data if g.compare_all(**regex)]
+            return lib.time, [g for g in lib.data if g.compare_all(**regex)]
